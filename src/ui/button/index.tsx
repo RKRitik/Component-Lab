9cdecd89371fc6524829126051dda84347877c04
ritikkhatri <ritik.khatri.i@dotpe.in>
2024-09-19 15:45:33 +0530

    fix typing for button component

--- src/ui/button/index.tsx
+++ src/ui/button/index.tsx
@@ -1,12 +1,11 @@
 type ButtonSize = "large" | "medium" | "small";
 
-type ButtonProps = {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   primary?: boolean;
-  onClick: Function;
   size: ButtonSize;
 };
 
-export const Button = ({ label, primary = true, size }: ButtonProps) => {
-  return <button className={`${primary ? 'text-blue ' : ''}${size === 'large' ? "p-24" : size === 'medium' ? 'p-22' : "p-18"}`}>{label}</button>;
+export const Button = ({ label, primary = true, size, ...rest }: ButtonProps) => {
+  return <button className={`${primary ? 'text-blue ' : ''}${size === 'large' ? "p-24" : size === 'medium' ? 'p-22' : "p-18"}`} {...rest}>{label}</button>;
 };
